79256cf372e5b23a7c8a618d7d25d8f803758770
naskafibdiscord <tony.burroni@gmail.com>
2026-09-12 18:53:19 +0200

    Initialize Discord bot with ping command

--- bot.py
+++ bot.py
@@ -1,2 +1,22 @@
+import os
+import discord
+from discord.ext import commands
 
-print("Mon bot est prêt !")
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Connecté en tant que {bot.user}")
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("🏓 Pong !")
+
+bot.run(TOKEN)
